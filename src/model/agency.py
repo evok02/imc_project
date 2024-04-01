8ffa49f76866f407ec43785aae0cef85cce4a86b
Hlib Tereshchenko <23IMC10640@fh-krems.ac.at>
2024-04-01 14:10:34 +0200

    extended Newspaper class

---
+++ src/model/agency.py
@@ -0,0 +1,39 @@
+from typing import List, Union, Optional
+from flask import jsonify
+
+from .newspaper import Newspaper
+
+
+class Agency(object):
+    singleton_instance = None
+
+    def __init__(self):
+        self.newspapers: List[Newspaper] = []
+
+    @staticmethod
+    def get_instance():
+        if Agency.singleton_instance is None:
+            Agency.singleton_instance = Agency()
+
+        return Agency.singleton_instance
+
+    def add_newspaper(self, new_paper: Newspaper):
+        #TODO: assert that ID does not exist  yet (or create a new one)
+        for newspaper in self.newspapers:
+            if newspaper.paper_id == new_paper.paper_id:
+                return jsonify(f"Newspaper with ID {new_paper.paper_id} already exist, try another one")
+        else:
+            self.newspapers.append(new_paper)
+
+    def get_newspaper(self, paper_id: Union[int,str]) -> Optional[Newspaper]:
+        for paper in self.newspapers:
+            if paper.paper_id == paper_id:
+                return paper
+        return None
+
+    def all_newspapers(self) -> List[Newspaper]:
+        return self.newspapers
+
+    def remove_newspaper(self, paper: Newspaper):
+        self.newspapers.remove(paper)
+
